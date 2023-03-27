c1cf971861169b865604268ee2fa69537e5c35af
阴智鹏 <1961654597@qq.com>
2023-03-27 16:38:35 +0800

    redis init

--- app-manage-commons/database/src/main/java/com/example/database/po/mypo/Test1.java
+++ app-manage-commons/database/src/main/java/com/example/database/po/mypo/Test1.java
@@ -1,11 +1,16 @@
 package com.example.database.po.mypo;
 
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("test1")
 public class Test1 {
 
+    @TableId
     private String name;
+
     private String sex;
 }
--- app-manage-server/src/main/java/com/example/server/controller/TestController.java
+++ app-manage-server/src/main/java/com/example/server/controller/TestController.java
@@ -1,21 +1,21 @@
 package com.example.server.controller;
 
 import com.example.database.po.mypo.Test1;
-import com.example.server.service.Impl.Tes1ServiceImpl;
 import com.example.server.service.Test1Service;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
+ * 测试
  * @Author yin
  * @Date 2023/3/5 11:31
  */
-
-
 @RequestMapping("/test")
 @RestController
 public class TestController {
@@ -23,15 +23,26 @@ public class TestController {
     @Resource
     private Test1Service test1Service;
 
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+    /**
+     * a接口
+     * @return
+     */
     @GetMapping("/a")
     public List<Test1> test1List(){
         return test1Service.testAll();
     }
 
-
+    /**
+     * b接口
+     * @param str 随便
+     * @return
+     */
     @GetMapping("/b")
-    public String str(){
-        return "连接成功";
+    public String str(String str){
+        redisTemplate.opsForValue().set("1414", "552552");
+        return "连接成功:"+str;
     }
 
 }
